4432b77d89b12293c88414914a5149f63dd3f2af
Juditbr92 <juditbardonromero@gmail.com>
2024-02-02 19:08:18 +0100

    cambios reto 2 dia 2

--- Dia2/nuevoReto3.js
+++ Dia2/nuevoReto3.js
@@ -1,14 +1,15 @@
+const res = require('express/lib/response');
 var fs = require('fs/promises');
 
 const readline = require('readline')
 
 function pregunta(pregunta) {
-    const question = new Promise ( (resolve, reject)  => {
-        const rl = readline.createInterface( {
+    const question = new Promise((resolve, reject) => {
+        const rl = readline.createInterface({
             input: process.stdin,
             output: process.stdout
         });
-        rl.question( pregunta, (respuesta) => {
+        rl.question(pregunta, (respuesta) => {
             resolve(respuesta);
             rl.close();
         });
@@ -16,16 +17,21 @@ function pregunta(pregunta) {
     return question
 }
 
-async function asyncAwait (){
+async function asyncAwait() {
     try {
         const respuestaName = await pregunta("What is your name?");
         const respuestaSurname = await pregunta("What is your surname?");
         const respuestaAge = await pregunta("How old are you?");
-        console.log(`${respuestaName}, ${respuestaSurname}, ${respuestaAge}`)
-    } catch (error) {
-        console.log(error)
+        let newPerson = { respuestaName, respuestaSurname, respuestaAge };
+        await fs.writeFile('newTest2.json', JSON.stringify(newPerson));
+        console.log('El archivo se ha creado correctamente');
+        let res = await fs.readFile('newTest2.json', 'utf-8');
+        console.log(JSON.parse(res))
+    }        
+    catch (error) {
+    console.log(error)
     }
-}
+} 
 
 asyncAwait()
 
