62fc6a33f1ec60671af55e48b9c94ec62efaba0c
Juditbr92 <juditbardonromero@gmail.com>
2024-02-13 19:38:14 +0100

    cambios dia 3

--- Dia3/src/models/bookClass.js
+++ Dia3/src/models/bookClass.js
@@ -1,8 +1,17 @@
 export class Book{
+    id_book!;
+    title!;
+    type!;
+    author!;
+    price!;
+    photo!;
 
-    constructor(id, title, author){
-        this.id = id;
+    constructor(id_book = 0, title, type, author, price, photo){
+        this.id_book = id_book;
         this.title = title;
+        this.type = type;
         this.author = author;
+        this.price = price;
+        this.photo = photo;
     }
 }
